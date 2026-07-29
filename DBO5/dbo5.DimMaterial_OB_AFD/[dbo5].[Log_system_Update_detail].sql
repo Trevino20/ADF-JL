92ef066bb2fbc47d7e26634068fadd9de4de771c
Anshuman Chawhan <159607339+XAnshumanX@users.noreply.github.com>
2026-07-29 14:16:15 +0530

    Create [dbo5].[Log_system_Update_detail].sql

---
+++ DBO5/dbo5.DimMaterial_OB_AFD/[dbo5].[Log_system_Update_detail].sql
@@ -0,0 +1,78 @@
+/****** Object:  StoredProcedure [dbo5].[Log_system_Update_detail]    Script Date: 29-07-2026 14:15:56 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE    PROC [dbo5].[Log_system_Update_detail] 
+@log_ID INT,
+@Inserted_row INT,
+@ModuleName VARCHAR(100),
+@activity_type NVARCHAR(100),
+@Remarks NVARCHAR(500) NULL,
+@Destination_type VARCHAR(100) NULL,
+@deleted_rows INT 
+AS
+BEGIN
+BEGIN TRANSACTION
+BEGIN TRY 
+
+
+BEGIN
+
+BEGIN
+UPDATE dbo5.Log_Table
+SET Inserted_Row=@Inserted_row,
+Activity_End_Time=DATEADD(MINUTE,330,GETDATE()),
+Run_Status='Completed',
+Remarks=@Remarks,
+Destination_Type=@Destination_type,
+Deleted_rows=@deleted_rows
+WHERE 
+Log_ID=@log_ID
+AND 
+ModuleName=@ModuleName
+AND 
+Activity_Type=@activity_type
+END
+
+
+BEGIN
+UPDATE dbo5.Log_Table
+SET Activity_Completion_Time_Taken_In_Min=DATEDIFF(SECOND,Activity_Start_Time,Activity_End_Time)/60.0
+WHERE 
+Log_ID=@log_ID
+AND 
+ModuleName=@ModuleName
+AND 
+Activity_Type=@activity_type
+END
+
+END
+
+
+COMMIT TRANSACTION;
+END TRY 
+BEGIN CATCH
+PRINT('Error Occurred'+ERROR_MESSAGE())
+ROLLBACK TRANSACTION;
+RETURN;
+END CATCH
+END
+GO
+
+
